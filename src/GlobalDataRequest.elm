d712e989f8541b696a524100320a2af2d289cded
Xavier Shay <contact@xaviershay.com>
2018-10-05 17:52:28 -0700

    Doc

--- src/GlobalDataRequest.elm
+++ src/GlobalDataRequest.elm
@@ -5,6 +5,15 @@ import Http
 import Status exposing (..)
 
 
+
+-- INTERESTING: This module encapsulates the different types of global data
+-- that are "available" for pages to request, as well as mappings from the
+-- abstract request type (`Profile`) to details that can be wrapped up by the
+-- `Main` module to handle the requests. It's possible that a level of
+-- indirection could be removed here, and the pages (via a function) refer
+-- directly to the thing returned by `toRequests` rather than a `Request`.
+
+
 type GlobalDataRequest
     = CompletedProfileRequest (Result Http.Error Data.Profile.Profile)
 
